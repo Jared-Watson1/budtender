e8e606a9011ceb0829137433e5990e5acc1fcbf8
Jared Watson <jared.watson@emory.edu>
2023-04-20 18:45:40 -0400

    Update embedding.py

--- embedding.py
+++ embedding.py
@@ -1,18 +1,11 @@
-from openai.embeddings_utils import get_embedding
 from openai.embeddings_utils import (
     get_embedding,
     cosine_similarity,
-    distances_from_embeddings,
-    tsne_components_from_embeddings,
-    chart_from_components,
-    indices_of_nearest_neighbors_from_distances,
 )
 import dotenv
 import os
 import openai
 import pandas as pd
-import pickle
-import tiktoken
 import numpy as np
 
 dotenv.load_dotenv()
@@ -24,27 +17,6 @@ embedding_encoding = "cl100k_base"  # this the encoding for text-embedding-ada-0
 max_tokens = 8000  # the maximum for text-embedding-ada-002 is 8191
 top_n = 1000
 input_datapath = "data/wiki_data.csv"
-
-
-def createEmbeddings(inputFile=input_datapath):
-
-    df = pd.read_csv(inputFile)
-    df = df[["title", "heading", "content", "tokens"]]
-    df = df.dropna()
-    df["combined"] = (
-        "Heading: " + df.heading.str.strip() + "; Content: " + df.content.str.strip()
-    )
-
-    encoding = tiktoken.get_encoding(embedding_encoding)
-
-    # omit reviews that are too long to embed
-    df["n_tokens"] = df.combined.apply(lambda x: len(encoding.encode(x)))
-    df = df[df.n_tokens <= max_tokens].tail(top_n)
-    print(len(df))
-    df["embedding"] = df.combined.apply(
-        lambda x: get_embedding(x, engine=embedding_model))
-    df.to_csv("data/pm_embeddings.csv")
-
 
 def filter(df, inp, n=3, pprint=False):
     df.head()
@@ -69,8 +41,3 @@ def filter(df, inp, n=3, pprint=False):
             print()
 
     return pd.array(results).astype(str)
-
-# emb = pd.read_csv("data/pm_embeddings.csv")
-# createEmbeddings()
-# print(filter(emb, "cardiac arrest"))
-# context = filter(emb, "cardiac arrest").astype(str)
